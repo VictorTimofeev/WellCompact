9a5cb933874532d372743e3be8c66f392842f16c
VictorTimofeev <vptimofeev@yahoo.com>
2024-09-05 12:42:26 +0500

    new window
    Set new window

---
+++ New File.py
@@ -0,0 +1,11 @@
+from tkinter import *
+# Create an app window
+window = Tk()
+# Add the app name
+window.title('Поиск новых целей (уплотнение)')
+# set window size
+window.geometry('500x300')
+frame = Frame(window, padx = 10, pady =10)
+frame.pack(expand = True)
+window.mainloop()
+print('iiii')
