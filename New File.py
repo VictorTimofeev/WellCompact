94da65fe66e9183c042efae96bfdb9ea6982953a
VictorTimofeev <vptimofeev@yahoo.com>
2024-09-05 13:39:50 +0500

    Add a few lines

--- New File.py
+++ New File.py
@@ -1,3 +1,4 @@
+import tkinter
 from tkinter import *
 # Create an app window
 window = Tk()
@@ -6,7 +7,8 @@ window.title('Поиск новых целей (уплотнение)')
 # set window size
 window.geometry('500x300')
 # Add button for set files
-
-frame = Frame(window, padx = 10, pady =10)
-frame.pack(expand = True)
+readme = tkinter.Label(text = "Необходимо выбрать выгрузку из ПО РН-КИН, данные гис с информацией по всем интервалами перфорации")
+readme.pack()
+button = tkinter.Button(text = "Выберите файл с базой данных")
+button.pack()
 window.mainloop()
